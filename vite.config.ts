728225a2204e280768b5b47a50a9b643e54c60b4
Gowthamchand5102 <gouthamchand5102@gmail.com>
2025-04-02 18:01:18 +0530

    routes

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    historyApiFallback: true, // Add this line to handle SPA routing
   },
   plugins: [
     react(),
